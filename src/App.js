c2d08f37f1d3c2e170eecee60f5a61ca9a73cc5c
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-05-18 10:02:02 +0300

    updaing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Greet from "./Components/Revision1-20/Greet"
 function App() {
+  const name = "mohamed Ramadan";
   return (
     <div className="App">
+      <Greet name={name } />
+
     
     </div>
   );
