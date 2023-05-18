73859ec3e41301c3842fa14370b822727e4c6b2a
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-05-18 10:40:33 +0300

    Learn  react with no jsx

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from "./Components/Revision1-20/Greet";
 import Wellcom from './Components/Revision1-20/Wellcom';
+import Hello from './Components/Revision1-20/Hello';
 function App() { 
   return (
     <div className="App">
-      {/* <Greet name={name} content={ content} /> */}
-      <Wellcom/>
+      {/* <Greet name={name} content={ content} />
+      <Wellcom/> */}
+      <Hello/>
     
     </div>
   );
---
+++ src/Components/Revision1-20/Hello.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Hello = () => {
+    return (
+        React.createElement("div",{className:"GoodFather"},React.createElement("p",{className:"lucky"},"you good and you learn fast "))
+    )
+}
+
+
+export default Hello;
