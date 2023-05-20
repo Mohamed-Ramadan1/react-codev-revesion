40f9f9c04a6646523166d51d4011ba3577021457
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-05-20 20:41:27 +0300

    updating some files

--- src/App.js
+++ src/App.js
@@ -16,8 +16,6 @@ function App() {
   return (
     <div className="App">
       <FormClassHandling/>
-      
-
     </div>
   );
 }
--- src/Components/Revision20-40/FormClassHandling.js
+++ src/Components/Revision20-40/FormClassHandling.js
@@ -5,13 +5,54 @@ class FormClassHandling extends React.Component{
     constructor () {
         super();
 
-        this.state={}
+        this.state = {
+            userName: '',
+            password: '',
+            email:''
+        }
+
+    }
+    handelUserName=(e)=> {
+        this.setState({
+            userName: e.target.value, 
+        })
+    }
+    handelEmailChange=(e)=> {
+        this.setState({
+            email: e.target.value, 
+        },()=>console.log(this.state.email))
+    }
+
+    handelPasswordFaild=(e)=> {
+        this.setState({
+            password: e.target.value, 
+        },()=>console.log(this.state.password))
     }
 
     render() {
+       // const { userName, password, email } = this.state;
         return (
             <div>
-
+                <form>
+                    <label>UserName</label>
+                    <input
+                        type='text'
+                        value={this.state.userName}
+                        onChange={this.handelUserName}
+                    />
+                    <label>password</label>
+                    <input
+                        onChange={this.handelPasswordFaild}
+                        type='password'
+                        value={this.state.password}
+                    />
+                    <label>Email</label>
+                    <input
+                        onChange={this.handelEmailChange}
+                        type='emali'
+                        value={this.state.email}
+                    />
+                </form>
             </div>
         )
     }
