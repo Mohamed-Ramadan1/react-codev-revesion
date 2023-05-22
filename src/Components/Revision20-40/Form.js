022b9d26b74c66215c052b385c5124fecc4db490
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-05-22 10:10:04 +0300

    working with the fomrs

--- src/Components/Revision20-40/Form.js
+++ src/Components/Revision20-40/Form.js
@@ -5,12 +5,58 @@ class Form extends React.Component{
     constructor () {
         super();
 
-        this.state={}
+        this.state = {
+            userName: "",
+            comments: '',
+            topic:'react'
+        }
+    
     }
 
+    handelUserName = (ev) => {
+        this.setState({
+            userName:ev.target.value
+        })
+    }
+
+    handelTextArea = (e) => {
+        this.setState({
+            comments: e.target.value
+        })
+    }
+
+    handelTopics = (event) => {
+        this.setState({
+            topic:event.target.value
+        })
+    }
     render() {
         return (
-            <div></div>
+            <form>
+                <div>
+                <label>UserName</label>
+                <input
+                    type='text'
+                    value={this.state.userName}
+                    onChange={this.handelUserName} />
+                </div>
+
+                <div>
+                    <label>Comments</label>
+                    <textarea
+                        value={this.state.comments}
+                        onChange={this.handelTextArea}
+                    ></textarea>
+                </div>
+
+                <div>
+                    <select value={this.state.topic} onChange={this.handelTopics}>
+                        <option value='react'>React</option>
+                        <option value='vue'>vue</option>
+                        <option value='anguler'>anguler</option>
+                    </select>
+                </div>
+            </form>
 
         );
     }
