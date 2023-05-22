0502af52a6f91f504b4be7918a9dda7b08186284
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-05-22 17:56:13 +0300

    updating

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
-import Greet from "./Components/Revision1-20/Greet";
-import Wellcom from './Components/Revision1-20/Wellcom';
-import Hello from './Components/Revision1-20/Hello';
-import Massage from './Components/Revision1-20/Massage';
-import Counter from './Components/Revision1-20/Counter';
-import Functionclicks from './Components/Revision1-20/FunctionClicks';
-import BindEvent from './Components/Revision1-20/BindEventHandler';
-import Parent from './Components/Revision1-20/ParentComponent';
-import UserGreeting from './Components/Revision1-20/UserGreating';
-import NameList from './Components/Revision1-20/NameList.';
-import StyleSheet from './Components/Revision20-40/StyleSheet';
-import Inline from './Components/Revision20-40/Inline';
+import Form from './Components/Revision20-40/Form';
+
 function App() {
 
   return (
