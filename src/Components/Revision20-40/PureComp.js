1a01ed8cc35bd9e127a7d35c7568ab2843d2197e
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-03 11:09:05 +0300

    hey

---
+++ src/Components/Revision20-40/PureComp.js
@@ -0,0 +1,17 @@
+import React, { PureComponent } from "react";
+class PureComp extends PureComponent{
+    constructor () {
+        super();
+
+    }
+    render() {
+        return (
+            <div>
+                Your Compoent
+            </div>
+        )
+
+    }
+}
+
+export default PureComp;
