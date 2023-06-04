d7f8194c61c5cb81195b29a8bbb4ad31070df340
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-04 10:27:30 +0300

    new file added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import ClickRef from './Components/Revision20-40/ClickRef';
-import PureComp from './Components/Revision20-40/PureComp';
-import RefsDemo from './Components/Revision20-40/RefsDemo';
+
 
 function App() {
 
   return (
     <div className="App">
-      <ClickRef/>
 
     </div>
   );
