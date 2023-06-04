36fa8743d1efa963d99a65a2b03df81fff45baca
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-04 10:50:36 +0300

    updating the location and scopes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import FRPinputComponent from './Components/Revision20-40/FRParentInput'
 
 function App() {
 
   return (
     <div className="App">
-      <FRPinputComponent/>
 
     </div>
   );
