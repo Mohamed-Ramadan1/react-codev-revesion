54dfc073a6be982b407bd07e32eb3279163f7932
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-05 09:29:06 +0300

    updating some files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
-import ComponentC from './Components/Revision30-43/ComponentC';
-import ComponentF from './Components/Revision30-43/ComponentF';
-import { UserProvider } from './Components/Revision30-43/userContext';
+
 
 function App() {
 
   return (
     <div className="App">
-      <UserProvider value="MohamedRamadan">
-          <ComponentC/>
-      </UserProvider>
+  
     
     </div>
   );
