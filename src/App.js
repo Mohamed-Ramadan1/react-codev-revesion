fdf70bbdb5dba6f95d3984b6cad0d85942fe119a
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-11 12:17:37 +0300

    done sucssfully

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import CounterOne from './Components/Hooks61-70/CounterOne';
 import DataFectchingOne from './Components/Hooks61-70/DataFectchingOne';
+import DataFetchingTwo from './Components/Hooks61-70/DataFetchingTwo';
 
 function App() {
 
   return (
     <div className="App">
-      <DataFectchingOne/>
+      <DataFetchingTwo/>
     </div>
   );
 }
--- src/Components/Hooks61-70/DataFetchingTwo.js
+++ src/Components/Hooks61-70/DataFetchingTwo.js
@@ -1,8 +1,50 @@
-import React,{useEffect,useState} from 'react'
+import React,{useEffect,useReducer,useState} from 'react'
 import axios from 'axios'
+
+const init = {
+    loading: true,
+    comments: {},
+    error:''
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "data_sucsses" : return{
+            loading: false,
+            comments: action.payload,
+            error: ''
+        }
+
+        case "dataNot_Featching": return {
+            loading: false,
+            comments: {},
+            error: "Somthing went wrong"
+        }
+
+        default: return state
+    }
+}
+
+
+
+
 function DataFetchingTwo() {
+    const [state, disPatch] = useReducer(reducer, init)
+
+    useEffect(() => {
+        axios.get("https://jsonplaceholder.typicode.com/comments")
+            .then((res) => {
+                disPatch({type:"data_sucsses",payload:res.data})               
+
+            })
+            .catch(error => {
+                disPatch({type:'dataNot_Featching'})
+        })
+    },[])
     return (
         <div>
+            {state.loading ? "Loading the required Data" : state.comments.map(coment => <h2>{coment.email}</h2>)}
+            {state.error ? state.error : null}
         
         </div>
     )
