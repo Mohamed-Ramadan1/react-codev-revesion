1f55a72e8d9f1cb9c0a136a1f8e222462ca53737
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-11 11:44:15 +0300

    hello

---
+++ src/Components/Hooks61-70/DataFetchingTwo.js
@@ -0,0 +1,11 @@
+import React,{useEffect,useState} from 'react'
+import axios from 'axios'
+function DataFetchingTwo() {
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default DataFetchingTwo
