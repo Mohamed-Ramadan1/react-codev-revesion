5e44a4929c4384cd3cc4a03f5a426f1558cf4532
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-11 11:42:15 +0300

    funcy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import CounterOne from './Components/Hooks61-70/CounterOne';
+import DataFectchingOne from './Components/Hooks61-70/DataFectchingOne';
 
 function App() {
 
   return (
     <div className="App">
+      <DataFectchingOne/>
     </div>
   );
 }
