99e17d7e09985bed85979cea0938f929b12880a9
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-07-13 05:25:26 +0300

    The course setup

--- src/Components/Revision1-20/Person.js
+++
@@ -1,9 +0,0 @@
-import Reaact from 'react';
-
-function Persons () {
-    return (
-       
-        <div>{`Your Name is ${person.name} and your skills is ${person.skills}`}</div>
-    )
-}
-export default Persons;
