f94905bf7ae5662d0e39dd9fe608d0fd3d3f0297
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-07-14 12:21:00 +0300

    ending the logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,54 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 const App = () => {
-  return <div className="container"></div>;
+  const [step, setStep] = useState(3);
+
+  const prevClickHandler = () => {
+    step <= 1 ? setStep(1) : setStep((prev) => prev - 1);
+  };
+
+  const nextClickHandler = () => {
+    step >= 1 && step + 1 <= messages.length
+      ? setStep((prev) => prev + 1)
+      : setStep(step);
+  };
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        {messages.map((messag) => (
+          <div
+            key={messages.indexOf(messag)}
+            className={messages.indexOf(messag) + 1 === step ? "active" : ""}
+          >
+            {messages.indexOf(messag) + 1}
+          </div>
+        ))}
+      </div>
+      <p className="message">step-{`${step} : ${messages[step - 1]}`}</p>
+
+      <div className="buttons">
+        <button
+          style={{ background: "#7950f2", color: "#fff" }}
+          onClick={prevClickHandler}
+        >
+          Previouse
+        </button>
+        <button
+          style={{ background: "#7950f2", color: "#fff" }}
+          onClick={nextClickHandler}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
